d5555c3ee81cb1b83ce77d2fc044c845c9fecd54
LewisContreras <legiconba4@gmail.com>
2021-05-15 18:17:52 -0500

    Terminados requisitos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 
 import './App.css';
+import dbQuotes from "./dbQuotes.json"
+let randomInit = Math.floor(Math.random()*dbQuotes.length);
+let obInit = dbQuotes[randomInit]
+let textInit = obInit.quote
+let autInit = obInit.author
+
 
 function App() {
+  
+  
+
+  const handleClick = () =>{
+    let text = document.getElementById("text");
+
+    let author = document.getElementById("author");
+    let randomNumber = Math.floor(Math.random()*dbQuotes.length);
+    let randomObject = dbQuotes[randomNumber];
+    
+    text.textContent = `"${randomObject.quote}"`;
+    author.textContent =  `- ${randomObject.author}`
+    
+    
+    
+
+  }
+  
+
   return (
-    <div >
-      Holiwis
+    <div id="wrapper" >
+      <div id="quote-box" >
+        <p id="text" >{textInit}</p>
+        <p id="author" >{autInit}</p>
+        <button onClick={handleClick} id="new-quote" >new</button>
+        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank" >twitter</a>
+      </div>
     </div>
+
   );
 }
 
